cbf07f0510a1c6766245e85f177d8bc90baad847
tatsuya-hasebe <tatsuya.hasebe@a-saas.com>
2020-07-13 16:39:28 +0900

    extract functions

--- videostore.js
+++ videostore.js
@@ -1,40 +1,72 @@
 function statement (customer, movies) {
-  let totalAmount = 0;
-  let frequentRenterPoints = 0;
   let result = 'Rental Record for ' + customer.name + '\n'
+
   for (let r of customer.rentals) {
-    let movie = movies[r.movieID];
-    let thisAmount = 0;
-
-    switch (movie.code) {
-      case 'regular':
-        thisAmount = 2;
-        if (r.days > 2) {
-          thisAmount += (r.days - 2) * 1.5;
-        }
-        break;
-      case 'new':
-        thisAmount = r.days * 3;
-        break;
-      case 'childrens':
-        thisAmount = 1.5;
-        if (r.days > 3) {
-          thisAmount += (r.days - 3) * 1.5;
-        }
-        break;
-    }
-
-    frequentRenterPoints++;
-    if(movie.code === 'new' && r.days > 2) frequentRenterPoints++;
-
-    result += '\t' + movie.title + '\t' + thisAmount + '\n'
-    totalAmount += thisAmount;
+    result += '\t' + movieFor(r, movies).title + '\t' + amountFor(r, movies) + '\n'
   }
 
-  result += 'Amount owed is ' + totalAmount + '\n'
-  result += 'You earned ' + frequentRenterPoints + ' frequent renter points\n';
+  result += 'Amount owed is ' + totalAmount(customer.rentals, movies) + '\n'
+  result += 'You earned ' + totalFrequentRenterPoints(customer.rentals, movies) + ' frequent renter points\n';
 
   return result;
+}
+
+function movieFor(rental, movies) {
+  return movies[rental.movieID]
+}
+
+function amountFor(rental, movies) {
+  let thisAmount = 0;
+
+  switch (movieFor(rental, movies).code) {
+    case 'regular':
+      thisAmount = 2;
+      if (rental.days > 2) {
+        thisAmount += (rental.days - 2) * 1.5;
+      }
+      break;
+    case 'new':
+      thisAmount = rental.days * 3;
+      break;
+    case 'childrens':
+      thisAmount = 1.5;
+      if (rental.days > 3) {
+        thisAmount += (rental.days - 3) * 1.5;
+      }
+      break;
+  }
+
+  return thisAmount;
+}
+
+function totalAmount(rentals, movies) {
+  let totalAmount = 0;
+
+  for (let r of rentals) {
+    totalAmount += amountFor(r, movies);
+  }
+
+  return totalAmount;
+}
+
+function frequentRenterPointsFor(rental, movies) {
+  let frequentRenterPoints = 0;
+
+  frequentRenterPoints++;
+  if(movieFor(rental, movies).code === 'new' && rental.days > 2) frequentRenterPoints++;
+
+  return frequentRenterPoints;
+}
+
+function totalFrequentRenterPoints(rentals, movies) {
+  let frequentRenterPoints = 0;
+
+  for (let r of rentals) {
+
+    frequentRenterPoints += frequentRenterPointsFor(r, movies)
+  }
+
+  return frequentRenterPoints;
 }
 
 module.exports = {
